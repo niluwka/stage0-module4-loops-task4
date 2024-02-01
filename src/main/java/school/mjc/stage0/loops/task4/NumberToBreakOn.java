d3048f7e8bcfe4599c9b68e3cddf282153803830
nilufar <ninimamatova0403@gmail.com>
2024-02-01 16:02:53 +0500

    NumberToBreakOn

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        int x = 0;
+        if (toBreakWith>numberToGoUntil) {
+            System.out.println("iterating till the end" + x);
+        }
+        for (int i = 1; i <= numberToGoUntil; i++) {
+            System.out.println(i);
+            if (i == toBreakWith) {
+                break;
+            }
+            x = i;
+
+
+        }
+
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -3,5 +3,27 @@ package school.mjc.stage0.loops.task4;
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
 
+        int sum = 0;
+int x =0;
+
+ if (numberToSkip > lastInRow) {
+            System.out.println ( "number to skip is bigger then the last" );
+        } else if (numberToSkip < 0 && lastInRow < 0) {
+     System.out.println("last number in row is negative");
+ } else {
+
+     for (int i = numberToSkip; i <= lastInRow; i++){
+         if (i == numberToSkip) {
+             x = numberToSkip+lastInRow;
+             System.out.println ( "skipped sum is " + x);
+             continue;
+
+         }
+         sum = sum + i;
+
+     }System.out.println ( "counted sum is " + sum);
+ }
+
+
     }
 }
